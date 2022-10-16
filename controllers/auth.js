f1c54b02c0284bc8ea40d8fd82389e9c9970e50f
Timothy Roy <timothyroy94@protonmail.com>
2022-10-16 09:22:50 +0200

    auth.js edited

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,15 @@
 
 const User = require("../models/user")
-
+const {check, validationResult} = require("express-validator")
 
 
 exports.signup = (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
     const user = new User(req.body);
     user.save((err,user) => {
         if(err){
@@ -17,6 +23,28 @@ exports.signup = (req, res) => {
             email: user.email, 
             id: user._id
         })
+    })
+}
+
+exports.signin = (req,res) => {
+    const {email, password} = req.body;
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
+    User.findOne({email}, (err, user) => {
+        if(err){
+            res.status(400).json({
+                error: "USER email does not exist"
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and pass do not match"
+            })
+        }
     })
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router =  express.Router()
+const {check} = require('express-validator')
+const {signout, signup, signin} = require("../controllers/auth")
 
-const {signout, signup} = require("../controllers/auth")
 
-
-router.post("/signup", signup)
+router.post("/signup", [
+    check("name", "name should be at least 3 char").isLength({min: 3}),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be at least 3 char is required").isLength({min: 3})
+], signup)
+router.post("/signin", [
+    
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").isLength({min: 1})
+], signin)
 router.get("/signout", signout)
 
 module.exports = router;
